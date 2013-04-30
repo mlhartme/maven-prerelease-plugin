0dd241b2d805fb57e95dfc3c95b9451008688418
Michael Hartmeier <mlhartme@github.com>
2013-04-30 21:40:47 +0200

    dump unused classes

--- src/main/java/net/oneandone/maven/plugins/prerelease/change/VersionException.java
+++
@@ -1,7 +0,0 @@
-package net.oneandone.maven.plugins.prerelease.change;
-
-public class VersionException extends Exception {
-    public VersionException(String message) {
-        super(message);
-    }
-}
