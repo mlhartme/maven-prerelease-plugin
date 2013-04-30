941f0307f674311ce68fbb7c742b1f6d1bdd8984
Michael Hartmeier <mlhartme@github.com>
2013-04-30 21:33:51 +0200

    cleanup package structure

---
+++ src/main/java/net/oneandone/maven/plugins/prerelease/change/VersionException.java
@@ -0,0 +1,7 @@
+package net.oneandone.maven.plugins.prerelease.change;
+
+public class VersionException extends Exception {
+    public VersionException(String message) {
+        super(message);
+    }
+}
